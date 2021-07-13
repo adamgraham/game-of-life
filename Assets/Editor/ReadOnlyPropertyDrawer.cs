87bf9f25ff6be3227b11d47248e65372537b7374
Adam Graham <adam@zigurous.com>
2021-07-13 01:07:40 -0500

    Fix ReadOnly attributes not drawing label

--- Assets/Editor/ReadOnlyPropertyDrawer.cs
+++ Assets/Editor/ReadOnlyPropertyDrawer.cs
@@ -7,7 +7,7 @@ public class ReadOnlyPropertyDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         GUI.enabled = false;
-        EditorGUI.PropertyField(position, property);
+        EditorGUI.PropertyField(position, property, label);
         GUI.enabled = true;
     }
 
